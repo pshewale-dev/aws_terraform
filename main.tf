d6c301b5f84f998d5938240555179696f4e3424c
root <root@ip-172-31-44-119.ec2.internal>
2024-07-16 12:18:19 +0000

    Update subnet to Public

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_key_pair" "ansible_key" {
@@ -11,9 +11,32 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "ansible_sg" {
@@ -37,10 +60,10 @@ resource "aws_security_group" "ansible_sg" {
 }
 
 resource "aws_instance" "ansible_node" {
-  ami                         = "ami-036c2987dfef867fb" # Amazon Linux 2 AMI
+  ami                         = "ami-036c2987dfef867fb" # Update with valid AMI ID
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ansible_key.key_name
-  subnet_id                   = aws_subnet.main.id
+  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ansible_sg.id]
   associate_public_ip_address = true
 
@@ -50,10 +73,10 @@ resource "aws_instance" "ansible_node" {
 }
 
 resource "aws_instance" "ansible_host1" {
-  ami                         = "ami-036c2987dfef867fb" # Amazon Linux 2 AMI
+  ami                         = "ami-036c2987dfef867fb" # Update with valid AMI ID
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ansible_key.key_name
-  subnet_id                   = aws_subnet.main.id
+  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ansible_sg.id]
   associate_public_ip_address = true
 
@@ -63,10 +86,10 @@ resource "aws_instance" "ansible_host1" {
 }
 
 resource "aws_instance" "ansible_host2" {
-  ami                         = "ami-036c2987dfef867fb" # Amazon Linux 2 AMI
+  ami                         = "ami-036c2987dfef867fb" # Update with valid AMI ID
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ansible_key.key_name
-  subnet_id                   = aws_subnet.main.id
+  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ansible_sg.id]
   associate_public_ip_address = true
 
